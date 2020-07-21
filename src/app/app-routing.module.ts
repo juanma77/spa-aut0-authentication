559813a250cc1bb5d33105af0fd8d182df0a866c
Juan Manuel Lopez Santana <freakshield777@hotmail.com>
2020-07-21 17:55:44 -0500

    routes functionality added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PricesComponent } from './components/prices/prices.component';
+import { ProtectedComponent } from './components/protected/protected.component';
 
 
-const routes: Routes = [];
+const ROUTES: Routes = [
+
+  { path: 'home', component: HomeComponent },
+  { path: 'prices', component: PricesComponent },
+  { path: 'protected', component: ProtectedComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot( ROUTES )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
